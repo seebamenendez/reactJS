f7022f5203aea18fb815a4efddf0ac776abb3950
seebamenendez <seeba.menendez@gmail.com>
2021-12-29 23:18:28 -0300

    actualizando git

--- src/components/ItemListContainer/ItemListContainer.js
+++ src/components/ItemListContainer/ItemListContainer.js
@@ -1,16 +1,36 @@
-import React from 'react'
+//import {useState, useEffect} from 'react';
+import React from 'react';
 
-const ItemListContainer = ({item}) => {
+function ItemListContainer () {
 
-    const {nombre, precio, coleccion} = item;
+const prendas = [
+    { nombre: "Campera Origen", precio: 2500, coleccion: "Lucida", stock: 6},
+    { nombre: "Vestido NATIVO", precio: 2400, coleccion: "Lucida", stock: 2},
+    { nombre: "Camisa OVER", precio: 1990, coleccion: "Lucida", stock: 5},
+    { nombre: "Sobrecamisa GALOPE", precio: 2100, coleccion: "Lucida", stock: 4}   
+]
 
+const traerPrendas = new Promise((resuelto, rechazado) => {
+    setTimeout(() => {
+        resuelto(prendas);
+      }, 3000);
+    });
+  
+    traerPrendas
+      .then((result) => result)
+      .then((data) => console.log(data))
+      .catch((err) => err);
+  
     return (
-        <div>
-            <h1>{nombre}</h1>
-            <h2>{precio}</h2>
-            <h2>{coleccion}</h2>
-        </div>
-    )
+      <div>
+        <h1>Bienvenidos a Arauca</h1>
+        <h2>Comienza a comprar!</h2>
+      </div>
+    );
+    
+
+
+
 }
 
 export default ItemListContainer
