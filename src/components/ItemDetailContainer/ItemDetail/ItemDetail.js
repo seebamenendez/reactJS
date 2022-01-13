ef137451eb7672d71427c42b344da6d2b7df2cb6
seebamenendez <seeba.menendez@gmail.com>
2022-01-12 23:36:19 -0300

    actualizando git

--- src/components/ItemDetailContainer/ItemDetail/ItemDetail.js
+++ src/components/ItemDetailContainer/ItemDetail/ItemDetail.js
@@ -1,11 +1,43 @@
+import React from 'react'
+import { useState } from 'react'
+import { Card } from 'react-bootstrap'
+import { Link } from 'react-router-dom'
+import ItemCount from '../../ItemCount/ItemCount'
 
+const ItemDetail = ({ product }) => {
 
-const ItemDetail = ({producto}) => {
+    const [show, setShow] = useState(true)
+
+    const { foto, nombre, precio, coleccion, stock } = product
+
+    const onAdd = (contador) => {
+        setShow(false)
+        alert(`${contador}`)
+    }
+
     return (
         <div>
-            {producto.titulo}
-            {producto.precio}
+            <Card style={{ width: '18rem', margin: '20px' }}>
+                <Card.Img variant="top" src={foto} style={{ minHeight: '300px' }} />
+                <Card.Body>
+                    <Card.Title>{nombre}</Card.Title>
+                    <Card.Text>
+                        Coleccion:{coleccion}
+                    </Card.Text>
+                    <Card.Text>
+                        {precio}
+                    </Card.Text>
+                </Card.Body>
+            </Card>
+
+            {show ? <ItemCount stock={stock} onAdd={onAdd} /> :
+                <div>
+                    <Link to='/cart'><button>Terminar la Compra</button></Link>
+                    <Link to='/'><button>Seguir Comprando</button></Link>
+                </div>}
+
         </div>
+
     )
 }
 
--- src/productos.jsx
+++ src/productos.jsx
@@ -2,74 +2,98 @@ export const productos = [
     {
         precio: 2690,
         id: "1",
-        titulo: "Campera ORIGEN",
-        foto: "multimedia/newIn/IMG_001.JPG"
+        nombre: "Campera ORIGEN",
+        foto: "./multimedia/newIn/IMG_001.JPG",
+        stock: "9",
+        coleccion: "Lucida"
     },
     {
         precio: 2400,
         id: "2",
-        titulo: "Vestido NATIVO",
-        foto: "multimedia/newIn/IMG_003.JPG"
+        nombre: "Vestido NATIVO",
+        foto: "./multimedia/newIn/IMG_003.JPG",
+        stock: "9",
+        coleccion: "Lucida"
     },
     {
         precio: 1990,
         id: "3",
-        titulo: "Camisa OVER",
-        foto: "multimedia/newIn/IMG_004.JPG"
+        nombre: "Camisa OVER",
+        foto: "./multimedia/newIn/IMG_004.JPG",
+        stock: "9",
+        coleccion: "Lucida"
     },
     {
         precio: 1700,
         id: "4",
-        titulo: "Básica Algodón",
-        foto: "multimedia/newIn/IMG_005.JPG"
+        nombre: "Básica Algodón",
+        foto: "./multimedia/newIn/IMG_005.JPG",
+        stock: "9",
+        coleccion: "Lucida"
     },
     {
         precio: 2390,
         id: "5",
-        titulo: "Sobrecamisa - PANTS",
-        foto: "multimedia/newIn/IMG_006.JPG"
+        nombre: "Sobrecamisa - PANTS",
+        foto: "./multimedia/newIn/IMG_006.JPG",
+        stock: "9",
+        coleccion: "Lucida"
     },
     {
         precio: 2690,
         id: "6",
-        titulo: "Campera ORIGEN",
-        foto: "multimedia/newIn/IMG_002.JPG"
+        nombre: "Campera ORIGEN",
+        foto: "./multimedia/newIn/IMG_002.JPG",
+        stock: "9",
+        coleccion: "Lucida"
     },
     {
         precio: 2390,
         id: "7",
-        titulo: "Sobrecamisa - PANTS",
-        foto: "multimedia/newIn/IMG_007.JPG"
+        nombre: "Sobrecamisa - PANTS",
+        foto: "./multimedia/newIn/IMG_007.JPG",
+        stock: "9",
+        coleccion: "Origen"
     },
     {
         precio: 2100,
         id: "8",
-        titulo: "Sobrecamisa GALOPE",
-        foto: "multimedia/newIn/IMG_008.JPG"
+        nombre: "Sobrecamisa GALOPE",
+        foto: "./multimedia/newIn/IMG_008.JPG",
+        stock: "9",
+        coleccion: "Origen"
     },
     {
         precio: 2390,
         id: "9",
-        titulo: "Pantalón ORION",
-        foto: "multimedia/newIn/IMG_009.JPG"
+        nombre: "Pantalón ORION",
+        foto: "./multimedia/newIn/IMG_009.JPG",
+        stock: "9",
+        coleccion: "Transitar"
     },
     {
         precio: 2690,
         id: "14",
-        titulo: "Campera ORIGEN",
-        foto: "multimedia/newIn/IMG_001.JPG"
+        nombre: "Campera ORIGEN",
+        foto: "./multimedia/newIn/IMG_001.JPG",
+        stock: "9",
+        coleccion: "Origen"
     },
     {
         precio: 2690,
         id: "10",
-        titulo: "Campera ORIGEN",
-        foto: "multimedia/newIn/IMG_002.JPG"
+        nombre: "Campera ORIGEN",
+        foto: "./multimedia/newIn/IMG_002.JPG",
+        stock: "9",
+        coleccion: "Transitar"
     },
     {
         precio: 2400,
         id: "12",
-        titulo: "Vestido NATIVO",
-        foto: "multimedia/newIn/IMG_003.JPG"
+        nombre: "Vestido NATIVO",
+        foto: "./multimedia/newIn/IMG_003.JPG",
+        stock: "9",
+        coleccion: "Transitar"
     }
 ]
 
