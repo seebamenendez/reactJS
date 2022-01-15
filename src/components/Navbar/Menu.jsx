a9bd2a2ac6d4177dae416172056ca0257f61e6a8
seebamenendez <seeba.menendez@gmail.com>
2022-01-15 19:51:04 -0300

    Add files via upload

--- src/components/Navbar/Menu.jsx
+++ src/components/Navbar/Menu.jsx
@@ -1,6 +1,8 @@
-import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Widget from './Widget';
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Widget from "./Widget";
+import { Link } from "react-router-dom";
+//import Cart from "../Cart/cart";
 
 function Menu() {
   return (
@@ -9,78 +11,29 @@ function Menu() {
         <div className="app">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
             <li className="nav-item">
-              <a
-                className="nav-link text-success activo"
-                aria-current="page"
-                href="index.html"
-              >
+              <Link className="nav-link text-success" to="/">
                 Inicio
-              </a>
+              </Link>
             </li>
             <li className="nav-item">
-              <a className="nav-link text-success" href="pages/nosotros.html">
-                Nosotros
-              </a>
+              <Link className="nav-link text-success" to="/categoria/Origen">
+                Origen{" "}
+              </Link>
             </li>
             <li className="nav-item">
-              <a className="nav-link text-success" href="pages/arauquitos.html">
-                Arauquitos
-              </a>
+              <Link className="nav-link text-success" to="/categoria/Transitar">
+                Transitar{" "}
+              </Link>
             </li>
             <li className="nav-item">
-              <a className="nav-link text-success" href="pages/newIn.html">
-                Shop
-              </a>
+              <Link className="nav-link text-success" to="/categoria/Lucida">
+                Lucida{" "}
+              </Link>
             </li>
-            <li className="nav-item dropdown">
-              <a
-                className="nav-link dropdown-toggle text-success"
-                href="/#"
-                id="navbarDropdown"
-                role="button"
-                data-bs-toggle="dropdown"
-                aria-expanded="false"
-              >
-                Temporadas
-              </a>
-              <ul
-                className="dropdown-menu text-success"
-                aria-labelledby="navbarDropdown"
-              >
-                <li>
-                  <a className="dropdown text-success" href="pages/origen.html">
-                    Orígen - AW ´21
-                  </a>
-                </li>
-                <li>
-                  <a
-                    className="dropdown text-success"
-                    href="pages/transitar.html"
-                  >
-                    Transitar - SS ´21
-                  </a>
-                </li>
-                <li>
-                  <a className="dropdown text-success" href="pages/audaz.html">
-                    Audaz - AW ´20
-                  </a>
-                </li>
-                <li>
-                  <a className="dropdown text-success" href="pages/lucida.html">
-                    Lúcida - SS ´20
-                  </a>
-                </li>
-              </ul>
-            </li>
-            <li className="nav-item">
-              <a
-                className="nav-link text-success"
-                href="pages/agregarCliente.html"
-              >
-                Inscribirse
-              </a>
-            </li>
-            <Widget />
+            <Link className="nav-link text-success" to='/cart'>
+              <Widget />
+            </Link>
+            
           </ul>
         </div>
       </nav>
@@ -88,5 +41,4 @@ function Menu() {
   );
 }
 
-
-export default Menu
+export default Menu;
