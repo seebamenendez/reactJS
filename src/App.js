2e65f0e13d4852807a3c8a1ace335bd5c88c1083
seebamenendez <seeba.menendez@gmail.com>
2022-01-15 18:56:40 -0300

    se vuelve a subir
    Se vuelve a subir ya que no habian quedado actualizado los archivos

--- src/App.js
+++ src/App.js
@@ -3,17 +3,25 @@ import Menu from './components/Navbar/Menu';
 import ItemListContainer from './components/ItemListContainer/ItemListContainer';
 //import ItemCount from './components/ItemCount/ItemCount';
 import ItemDetailContainer from './components/ItemDetailContainer/ItemDetailContainer';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import Cart from './components/Cart/cart'
 
 function App() {
 
   //jsx
   return(
+    <BrowserRouter>
     <div className="App" onClick ={ ()=>console.log() }>
       <h1>Arauca</h1>
       <Menu />
-      <ItemListContainer greeting='Bienvenidos a Arauca'/>
-      <ItemDetailContainer />
+      <Routes>
+        <Route exact path='/' element={<ItemListContainer greeting='Bienvenidos a Arauca'/>} />
+        <Route exact path='/categoria/:idCategoria' element={<ItemListContainer greeting='Bienvenidos a Arauca'/>} />
+        <Route exact path='/detalle/:detalleId' element={<ItemDetailContainer />} />
+        <Route exact path='/cart' element={<Cart />} />
+      </Routes>
     </div>
+    </BrowserRouter>
   )
 }
 
---
+++ src/components/Cart/cart.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Cart = () => {
+    return (
+        <div>
+            <h1>Lo siento, no existe aún el carrito</h1>
+        </div>
+    )
+}
+
+export default Cart
